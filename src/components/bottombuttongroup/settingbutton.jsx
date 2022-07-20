ddecc9cdb2540ca34f0eab036bd6a23cca2f4b5f
Austin1207 <austindog1207@gmail.com>
2022-07-20 10:23:59 +0800

    Fix_Setting

--- src/components/bottombuttongroup/settingbutton.jsx
+++ src/components/bottombuttongroup/settingbutton.jsx
@@ -14,17 +14,24 @@ const buttonsStyle = {
     "&:disabled": {backgroundColor: '#ffffff', color: '#b9bbbc'}
   };
 
-export default function SettingButton(){
+export default function SettingButton({state, sceneActions, itemsActions, projectActions}){
     const options = [
-        {name: 'Edit color mode', do: () => console.log('edit color mode')},
+        {name: 'Edit color mode', do: handleColorToggle},
         {name: 'Edit units', do: handleUnitToggle},
       ];
     const unitOptions = [
-        {name: 'Meters', do: () => console.log('Meters')},
-        {name: 'Feet', do: () => console.log('Feet')},
+        {name: 'Meters', do: () => ScaleSet1()},
+        {name: 'Feet', do: () => ScaleSet2()},
     ];
+    const colorOptions = [
+        {name: 'Light Mode', do: () => ColorTest1()},
+        {name: 'Dark Mode', do: () => ColorTest2()},
+    ];
+
+
     const [open, setOpen] = React.useState(false);
     const [unitOpen, setUnitOpen] = React.useState(false);
+    const [colorOpen, setColorOpen] = React.useState(false);
     const anchorRef = React.useRef(null);
     const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -36,6 +43,11 @@ export default function SettingButton(){
         setAnchorEl(event.currentTarget);
         setUnitOpen((prevOpen) => !prevOpen);
     };
+
+    function handleColorToggle(event) {
+        setAnchorEl(event.currentTarget);
+        setColorOpen((prevOpen) => !prevOpen);
+    };
     
     const handleClose = (event) => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) {
@@ -43,8 +55,107 @@ export default function SettingButton(){
     }
     setAnchorEl(null);
     setUnitOpen(false);
+    setColorOpen(false);
     setOpen(false);
     };
+
+    const ClickAreaLegend = (AreaLegend) => {
+        var e = document.createEvent("MouseEvents");
+        e.initEvent("click", true, true);
+        AreaLegend.dispatchEvent(e);
+    }
+
+    const OpenAreaLegend = event => {
+        var AreaLegendClick = document.getElementById("AreaLegend_Button")
+        if (localStorage.getItem("AreaLegend_Button") == "Close"){
+            ClickAreaLegend(AreaLegendClick);
+        }
+    }
+
+    function ScaleSet1() {
+        localStorage.setItem("Unit", "m")
+        localStorage.setItem("UnitName", "Meters")
+        }
+    
+    function ScaleSet2() {
+        localStorage.setItem("Unit", "ft")
+        localStorage.setItem("UnitName", "Feets")
+    }
+
+    function ColorTest1() {
+        localStorage.setItem("AreaLegend_Backgorund1","#ffffff")
+        localStorage.setItem("AreaLegend_Icon_Open","#222222")
+        localStorage.setItem("AreaLegend_Icon_Close","#222222")
+        localStorage.setItem("AreaLegend_Word","#222222")
+    
+        OpenAreaLegend()
+    
+        setTimeout(() => {
+            document.getElementById("IconConstructionLine_Dark").style.display = "none"
+            document.getElementById("IconObstacleLine_Dark").style.display = "none"
+            document.getElementById("IconNoCamLine_Dark").style.display = "none"
+            document.getElementById("IconMustLine_Dark").style.display = "none"
+            document.getElementById("IconConstructionLine_Light").style.display = ""
+            document.getElementById("IconObstacleLine_Light").style.display = ""
+            document.getElementById("IconNoCamLine_Light").style.display = ""
+            document.getElementById("IconMustLine_Light").style.display = ""
+        
+            document.getElementById("AreaLegend_Background2").style.backgroundColor = "#ffffff"
+        
+            document.getElementById("IconConstructionLine_Word").style.color = "#222222"
+            document.getElementById("IconInterestLine_Word").style.color = "#222222"
+            document.getElementById("IconObstacleLine_Word").style.color = "#222222"
+            document.getElementById("IconNoCamLine_Word").style.color = "#222222"
+            document.getElementById("IconMustLine_Word").style.color = "#222222"
+    
+        
+            localStorage.setItem("ColorMode","Light")
+            sceneActions.selectLayer("layer1")
+            itemsActions.selectItem("layer1", "xFAw434Nm");
+        }, 200)
+    
+        setTimeout(() => {
+            projectActions.unselectAll();
+            sceneActions.selectLayer("layer2");
+        }, 250)
+    }
+    
+    function ColorTest2() {
+        localStorage.setItem("AreaLegend_Backgorund1","#222222")
+        localStorage.setItem("AreaLegend_Icon_Open","#ffffff")
+        localStorage.setItem("AreaLegend_Icon_Close","#ffffff")
+        localStorage.setItem("AreaLegend_Word","#ffffff")
+    
+        OpenAreaLegend()
+    
+        setTimeout(() => {
+            document.getElementById("IconConstructionLine_Light").style.display = "none"
+            document.getElementById("IconObstacleLine_Light").style.display = "none"
+            document.getElementById("IconNoCamLine_Light").style.display = "none"
+            document.getElementById("IconMustLine_Light").style.display = "none"
+            document.getElementById("IconConstructionLine_Dark").style.display = ""
+            document.getElementById("IconObstacleLine_Dark").style.display = ""
+            document.getElementById("IconNoCamLine_Dark").style.display = ""
+            document.getElementById("IconMustLine_Dark").style.display = ""
+        
+            document.getElementById("AreaLegend_Background2").style.backgroundColor = "#222222"
+        
+            document.getElementById("IconConstructionLine_Word").style.color = "#ffffff"
+            document.getElementById("IconInterestLine_Word").style.color = "#ffffff"
+            document.getElementById("IconObstacleLine_Word").style.color = "#ffffff"
+            document.getElementById("IconNoCamLine_Word").style.color = "#ffffff"
+            document.getElementById("IconMustLine_Word").style.color = "#ffffff"
+        
+            localStorage.setItem("ColorMode","Dark")
+            sceneActions.selectLayer("layer1")
+            itemsActions.selectItem("layer1", "xFAw434Nm");
+        }, 200)
+    
+        setTimeout(() => {
+            projectActions.unselectAll();
+            sceneActions.selectLayer("layer2");
+        }, 250)
+    }
 
     return (
         <div>
@@ -105,6 +216,37 @@ export default function SettingButton(){
                     <ClickAwayListener onClickAway={handleClose}>
                     <MenuList id="unit-button-menu" autoFocusItem>
                         {unitOptions.map((option, index) => (
+                        <MenuItem
+                            key={option.name}
+                            //selected={index === selectedIndex}
+                            //onClick={(event) => handleMenuItemClick(event, index)}
+                            onClick={option.do}
+                            style = {{cursor: 'url("https://cursor.s3.ap-northeast-1.amazonaws.com/select.png") 13.5 4.5,pointer'}}
+                        >
+                            {option.name}
+                        </MenuItem>
+                        ))}
+                    </MenuList>
+                    </ClickAwayListener>
+                </Paper>
+                </Grow>
+            )}
+            </Popper>
+
+            <Popper
+            open={colorOpen}
+            anchorEl={anchorEl}
+            placement='left-end'
+            transition
+            >
+            {({ TransitionProps}) => (
+                <Grow
+                {...TransitionProps}
+                >
+                <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="color-button-menu" autoFocusItem>
+                        {colorOptions.map((option, index) => (
                         <MenuItem
                             key={option.name}
                             //selected={index === selectedIndex}
